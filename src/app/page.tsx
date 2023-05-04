594b531ea9d8ba0698954fa90bca9a41537228d3
Facundo Perez Montalvo <facundoperezmontalvo@gmail.com>
2023-05-04 16:27:30 -0300

    copy done, and UI getting there

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,60 @@
 "use client";
+import Image from "next/image";
+import React, { FormEvent, useState } from "react";
+import CopyImage from "/public/icon-copy.svg";
+interface IOptions {
+  uppercase: boolean;
+  numbers: boolean;
+  symbols: boolean;
+}
 
-import React from "react";
-
-const INITIAL_VALUES = {
+const INITIAL_VALUES: IOptions = {
   uppercase: false,
   numbers: false,
   symbols: false,
 };
 
-export default function Home() {
-  const [options, setOptions] = React.useState(INITIAL_VALUES);
-  const [password, setPassword] = React.useState("asdfff");
-  const [length, setLength] = React.useState(12);
-  function generatePassword(length, uppercase, numbers, symbols) {
+export default function Home(): JSX.Element {
+  const [copy, setCopy] = React.useState<boolean>(false);
+  const [options, setOptions] = React.useState<IOptions>(INITIAL_VALUES);
+  const [strength, setStrength] = useState<string>("weak");
+  const [password, setPassword] = useState<string>("asdfff");
+  const [length, setLength] = useState<number>(12);
+
+  React.useEffect(() => {
+    if (copy) {
+      setTimeout(() => {
+        setCopy(false);
+      }, 2000);
+    }
+  }, [copy]);
+
+  const handleCopy = (link: string): void => {
+    navigator.clipboard.writeText(link);
+    setCopy(true);
+  };
+
+  function checkStrength(password: string): void {
+    if (
+      password.length < 8 ||
+      options.uppercase === false ||
+      options.numbers === false ||
+      options.symbols === false
+    ) {
+      setStrength("weak");
+    } else if (password.length < 12) {
+      setStrength("medium");
+    } else {
+      setStrength("strong");
+    }
+  }
+
+  function generatePassword(
+    length: number,
+    uppercase: boolean,
+    numbers: boolean,
+    symbols: boolean
+  ): void {
     let charset = "abcdefghijklmnopqrstuvwxyz";
     if (uppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     if (numbers) charset += "0123456789";
@@ -22,11 +64,11 @@ export default function Home() {
     for (let i = 0; i < length; i++) {
       password += charset.charAt(Math.floor(Math.random() * charset.length));
     }
-
+    checkStrength(password);
     setPassword(password);
   }
 
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  function handleSubmit(e: FormEvent<HTMLFormElement>): void {
     e.preventDefault();
     generatePassword(
       length,
@@ -36,20 +78,35 @@ export default function Home() {
     );
   }
 
-  const optionsList = Object.keys(INITIAL_VALUES);
+  const optionsList: (keyof IOptions)[] = Object.keys(
+    INITIAL_VALUES
+  ) as (keyof IOptions)[];
+
   return (
     <main className="flex flex-col items-center justify-between h-screen">
       <section className="flex flex-col items-center justify-center w-full max-w-sm min-h-screen mx-auto space-y-3 align-center">
         <h1>Password Generator</h1>
         <div className="flex justify-between w-full p-4 bg-zinc-900">
           <span className="text-xl font-bold">{password}</span>
-          <button> copy</button>
+          <button>
+            <Image
+              src={CopyImage}
+              alt="Copy"
+              width={18}
+              height={18}
+              onClick={() => handleCopy(password)}
+              className="hover:fill-neutral-50"
+            />
+          </button>
         </div>
         <article className="flex w-full p-4 bg-zinc-900">
           <form className="flex flex-col w-full gap-3" onSubmit={handleSubmit}>
             <label htmlFor="length">
-              <div className="flex justify-between">
-                Character Length <span className="text-lime-200">{length}</span>
+              <div className="flex items-center justify-between">
+                Character Length{" "}
+                <span className="text-lg font-bold text-lime-200">
+                  {length}
+                </span>
               </div>
               <input
                 type="range"
@@ -58,14 +115,13 @@ export default function Home() {
                 max={15}
                 value={length}
                 onChange={(event) => {
-                  setLength(event.target.value);
+                  setLength(parseInt(event.target.value, 10));
                 }}
-                defaultValue="12"
                 className="w-full h-2 overflow-hidden appearance-none bg-zinc-800"
               />
             </label>
             {optionsList.map((option) => (
-              <div key={option}>
+              <div key={option} className="flex items-center">
                 <input
                   type="checkbox"
                   id={option}
@@ -77,18 +133,30 @@ export default function Home() {
                       [option]: event.target.checked,
                     });
                   }}
+                  className="mr-4 border-2 bg-[url('/public/icon-check.svg')] border-gray-300 cursor-pointer text-lime-200 bg-zinc-800 hover:border-lime-200 checked:bg-lime-200 checked:accent-red-900 active:bg-none"
                 />
                 <label htmlFor={option} className="capitalize">
                   Include {option}
                 </label>
               </div>
             ))}
-
+            <div className="flex items-center justify-between p-3 bg-black/30">
+              <span className="font-bold text-zinc-700">STRENGTH</span>
+              <div className="flex gap-2 text-gray-100 uppercase">
+                <span className="text-lg text-gray-100">{strength}</span>
+                <div className="flex gap-2">
+                  <div className="w-2 h-6 bg-red-900"></div>
+                  <div className="w-2 h-6 bg-red-900"></div>
+                  <div className="w-2 h-6 border"></div>
+                  <div className="w-2 h-6 border"></div>
+                </div>
+              </div>
+            </div>
             <button
               type="submit"
               className="w-full p-3 font-bold bg-lime-200 text-zinc-900"
             >
-              Generate password
+              GENERATE &gt;
             </button>
           </form>
         </article>
